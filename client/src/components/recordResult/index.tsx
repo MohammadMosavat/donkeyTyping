7968bf399daed0fdc7fc5bc803932d6014f15a11
MohammadMosavat <42.octmst@gmail.com>
2025-04-30 16:32:57 +0330

    clean code in record result component

--- client/src/components/recordResult/index.tsx
+++ client/src/components/recordResult/index.tsx
@@ -23,69 +23,7 @@ const RecordResult = ({
   isOver,
   wpm,
 }: RecordResultType) => {
-  const [data, setData] = useState<WpmRecord>();
-  const [loading, setLoading] = useState(true);
   const { user } = useUser();
-  useEffect(() => {
-    // Fetch user data from the API
-    const fetchRecords = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(
-          `http://localhost:5000/store_wpm?username=${localStorage.getItem(
-            "username"
-          )}`
-        );
-        setData(response.data);
-      } catch (err) {
-        console.log("Failed to fetch records.");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchRecords();
-  }, []);
-
-  const lastFiveRecordsChart = useMemo(() => {
-    if (loading) <Loading />;
-    if (!Array.isArray(data) || !data) return;
-    const lastFive = Array.isArray(data) && data?.slice(-5);
-    console.log("lastFive", lastFive);
-    const LastFiveRecordFilter = (label: string): number[] => {
-      if (Array.isArray(lastFive)) {
-        if (label == "wpm") return lastFive.map((rec: WpmRecord) => rec.wpm);
-        if (label == "inChar")
-          return lastFive.map((rec: WpmRecord) => rec.incorrect_char);
-        if (label == "corChar")
-          return lastFive.map((rec: WpmRecord) => rec.correct_char);
-      }
-      return [];
-    };
-
-    const datasets = [
-      {
-        label: "WPM",
-        data: LastFiveRecordFilter("wpm"),
-        backgroundColor: "rgba(75, 192, 192, 0.6)",
-      },
-      {
-        label: "Correct Characters",
-        data: LastFiveRecordFilter("inChar"),
-        backgroundColor: "rgba(153, 102, 255, 0.6)",
-      },
-      {
-        label: "Incorrect Characters",
-        data: LastFiveRecordFilter("corChar"),
-        backgroundColor: "rgba(255, 159, 64, 0.6)",
-      },
-    ];
-    const labels = ["R5", "R4", "R3", "R2", "R1"];
-    const title = "Last 5 Record";
-    return <LineChart labels={labels} datasets={datasets} title={title} />;
-  }, [data]);
-  // <LineChart labels={labels} datasets={datasets} title={title} />
-  console.log("last five record ", lastFiveRecordsChart);
 
   return (
     <motion.div
@@ -95,11 +33,6 @@ const RecordResult = ({
       transition={{ duration: 0.3 }}
       className="flex w-full gap-10 text-white p-4 !pb-0 sm:p-8 md:p-10"
     >
-      {/* <img
-        className="fixed top-0 left-0 right-0 blur-lg bottom-0 -z-30 scale-110 w-full h-screen"
-        src={"/images/bg10.jpg"}
-        alt="Background"
-      /> */}
       <section className="w-full flex flex-col items-center justify-between gap-4 sm:gap-6">
         <section className="flex justify-between items-start gap-4 sm:gap-6">
           <p className="font-JetBrainsMono text-primary text-center sm:text-left">
@@ -108,11 +41,6 @@ const RecordResult = ({
           <p className="font-JetBrainsMono text-primary text-center sm:text-left">
             Accuracy: {acc}%
           </p>
-
-          {/* Display correct and incorrect characters */}
-          {/* <p className="font-JetBrainsMono text-primary text-center sm:text-left">
-              Correct Characters: {corChar}
-            </p> */}
           <p className="font-JetBrainsMono text-primary text-center sm:text-left">
             Incorrect Characters: {inChar}
           </p>
