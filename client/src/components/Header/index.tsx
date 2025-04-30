b9ceaa3cbb307b3360108037bb1a933d246cf5a2
MohammadMosavat <42.octmst@gmail.com>
2025-04-30 17:28:30 +0330

    change the user profile ui in header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -16,27 +16,37 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
   const isTyping = useSelector((state: RootState) => state.isTyping.value);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   const close = () => {
     setIsOpen(false);
   };
 
-  const userProfile = useMemo(() => {
-    return (
-      user.length > 0 && (
-        <Link
-          onClick={() => close()}
-          className="capitalize bg-secondary transition-all duration-200 ease-in-out text-fourth rounded-full w-10 h-10 flex items-center justify-center hover:shadow-lg hover:scale-110 font-JetBrainsMono"
-          href={`/${user[0].username}/sort?filter=newest`}
-        >
-          {user[0].username?.split("")[0]}
-        </Link>
-      )
+  const yourHallLink = useMemo(() => {
+    const username = user[0]?.username
+    return user.length > 0 ? (
+      <NavLinks
+        onClick={() => close()}
+        className={`group [&>a>button]:!p-0 w-full md:w-auto ${isOpen &&
+          "!rounded-xl"} opacity-50 hover:opacity-100 bg-transparent ${
+          pathname === `/wefwe/sort`
+            ? "!opacity-100 [&_*]:stroke-2"
+            : ""
+        }`}
+        link={`/${username}/sort?filter=newest`}
+        value={"Yourhall"}
+      />
+    ) : (
+      <NavLinks
+        onClick={() => close()}
+        className={`group [&>a>button]:!p-0 w-full md:w-auto ${isOpen &&
+          "!rounded-xl"} opacity-50 hover:opacity-100 bg-transparent ${
+          pathname === `/register/signup` ? "!opacity-100 [&_*]:stroke-2" : ""
+        }`}
+        link={`/register/signup`}
+        value={"Signup"}
+      />
     );
-  }, [user]);
+  }, [user , pathname]);
+
   return (
     isTyping == "off" && (
       <>
@@ -57,15 +67,6 @@ const Header = () => {
             isOpen ? "h-screen !items-start !flex-col" : "h-fit self-start"
           } gap-4 items-center  justify-between md:rounded-2xl transition-all duration-300`}
         >
-          <section className="flex md:hidden  items-center max-md:gap-4 justify-between w-full">
-            <button className="left-4 top-4 z-20" onClick={toggleMenu}>
-              <ReactSVG
-                src="/svgs/menu.svg"
-                className="[&>div>svg]:size-7 [&_*]:stroke-primary"
-              />
-            </button>
-            {userProfile}
-          </section>
           <ul
             className={`md:flex justify-start gap-8 ${
               isOpen ? "flex flex-col !items-start" : "hidden items-center"
@@ -74,7 +75,7 @@ const Header = () => {
             <li className="w-full  md:w-auto">
               <NavLinks
                 onClick={() => close()}
-                className={`group [&>a>button]:!p-0 w-full md:w-auto ${isOpen &&
+                className={`group [&>button]:!p-0 w-full md:w-auto ${isOpen &&
                   "!rounded-xl"} opacity-50 hover:opacity-100 bg-transparent ${
                   pathname === "/" ? "!opacity-100 [&_*]:stroke-2" : ""
                 }`}
@@ -104,34 +105,8 @@ const Header = () => {
                 value={"Settings"}
               />
             </li>
+            <li className="w-full md:w-auto">{yourHallLink}</li>
           </ul>
-
-          {user.length > 0 ? (
-            <Link
-              className={`capitalize max-md:hidden bg-secondary transition-all duration-200 ease-in-out text-fourth rounded-xl w-8 h-8 flex items-center justify-center hover:shadow-lg hover:scale-110 font-JetBrainsMono  ${
-                pathname === `/${user[0].username}/sort`
-                  ? "!bg-fourth text-primary [&_*]:stroke-2"
-                  : ""
-              }`}
-              href={`/${user[0].username}/sort?filter=newest`}
-            >
-              {user[0].username?.split("")[0]}
-            </Link>
-          ) : (
-            <NavLinks
-              onClick={() => close()}
-              className={`group w-full md:w-auto ${isOpen &&
-                "!rounded-xl"} p-2 [&>button>p]:hidden hover:bg-fourth ${
-                pathname === "/register/signup" ||
-                pathname === "/register/login"
-                  ? "!bg-fourth [&_*]:stroke-2"
-                  : ""
-              }`}
-              iconSrc="/svgs/user.svg"
-              link="/register/signup"
-              value={"Sign up"}
-            />
-          )}
         </motion.header>
       </>
     )
